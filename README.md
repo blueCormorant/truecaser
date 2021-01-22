50cf9252cf9f2fca40f1b644c1a115d97a70686b
ldelsignore <ldelsignore@fordham.edu>
2021-01-22 18:39:54 -0500

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Predict and restore text casing
+# Truecaser
+
+A set of language modeling tools that can be used to predict and restore text based case information.
+
+## Prerequisites
+- Python 3
+- [crfsuite](https://github.com/downloads/chokkan/crfsuite/crfsuite-0.12-x86_64.tar.gz)
+
+
+## Installing
+`pip install truecaser`
+
+
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as _file:
+    long_description = _file.read()
+
 setup(
 	name="truecaser",
 	version="0.0.5",
@@ -13,7 +16,8 @@ setup(
 	package_dir={"": "truecaser"},
 	classifiers=[
 		'Programming Language :: Python :: 3',
-	]
+	],
+	long_description=long_description,
+	long_description_content_type="text/markdown"
 )
-
 
