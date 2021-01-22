4c762d958edba4893992bbf1cf4abc53d0843cef
ldelsignore <ldelsignore@fordham.edu>
2021-01-22 18:49:35 -0500

    Update pkg version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as _file:
 
 setup(
 	name="truecaser",
-	version="0.0.5",
+	version="0.0.6",
 	description="Predict and restore word casing",
 	py_modules=[
 		"truecaser",
